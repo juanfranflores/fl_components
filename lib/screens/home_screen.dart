35ce5bacafec1ee9c9aa936ebe364c7fa6b8aac1
juanfranflores <juan27fp@gmail.com>
2023-01-31 13:07:05 -0300

    HomeScreen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -5,9 +5,24 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('HomeScreen'),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Componentes en Flutter'),
+        backgroundColor: Colors.indigo,
+        elevation: 0,
+      ),
+      body: ListView.separated(
+        itemCount: 10,
+        itemBuilder: (BuildContext context, int index) {
+          return ListTile(
+            leading: const Icon(Icons.access_time_outlined),
+            title: const Text('nombre de ruta'),
+            onTap: () {},
+          );
+        },
+        separatorBuilder: (BuildContext _, int __) {
+          return const Divider();
+        },
       ),
     );
   }
