7fa17823626af588f91fb8feaec259bad2e8894f
juanfranflores <juan27fp@gmail.com>
2023-02-03 12:26:35 -0300

    FadeInImage

--- lib/widgets/custom_card_type_2.dart
+++ lib/widgets/custom_card_type_2.dart
@@ -6,15 +6,30 @@ class CustomCardType2 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card(
-        elevation: 10,
-        child: Column(
-          children: const [
-            FadeInImage(
-              image: NetworkImage(
-                  'https://photographylife.com/wp-content/uploads/2017/01/What-is-landscape-photography.jpg'),
-              placeholder: AssetImage('assets/jar-loading.gif'),
+      clipBehavior: Clip.antiAlias,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+      elevation: 10,
+      child: Column(
+        children: [
+          const FadeInImage(
+            image: NetworkImage(
+                'https://photographylife.com/wp-content/uploads/2017/01/What-is-landscape-photography.jpg'),
+            placeholder: AssetImage('assets/jar-loading.gif'),
+            height: 350,
+            width: double.infinity,
+            fit: BoxFit.fitHeight,
+          ),
+          Container(
+            alignment: AlignmentDirectional.centerEnd,
+            padding: const EdgeInsets.only(
+              right: 20,
+              top: 20,
+              bottom: 10,
             ),
-          ],
-        ));
+            child: const Text("Image description"),
+          )
+        ],
+      ),
+    );
   }
 }
