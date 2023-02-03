729fa46a4d1d0eddea606f9d856f1dea5f8d4fb5
juanfranflores <juan27fp@gmail.com>
2023-02-03 10:08:08 -0300

    textButtonTheme style

--- lib/themes/app_theme.dart
+++ lib/themes/app_theme.dart
@@ -4,12 +4,16 @@ class AppTheme {
   static const Color primary = Colors.indigo;
 
   static final ThemeData lightTheme = ThemeData.light().copyWith(
-      primaryColor: Colors.indigo,
-      appBarTheme: const AppBarTheme(
-        backgroundColor: primary,
-        centerTitle: true,
-        elevation: 0,
-      ));
+    primaryColor: Colors.indigo,
+    appBarTheme: const AppBarTheme(
+      backgroundColor: primary,
+      centerTitle: true,
+      elevation: 0,
+    ),
+    textButtonTheme: TextButtonThemeData(
+      style: TextButton.styleFrom(foregroundColor: primary),
+    ),
+  );
   static final ThemeData darkTheme = ThemeData.dark().copyWith(
     primaryColor: Colors.indigo,
     appBarTheme: const AppBarTheme(
