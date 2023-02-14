448013e84f5193d0fbdebaec2e4d3b1c78df1351
juanfranflores <juan27fp@gmail.com>
2023-02-14 13:09:12 -0300

    agrego campos a la configuracion del custom input

--- lib/widgets/custom_input_field.dart
+++ lib/widgets/custom_input_field.dart
@@ -4,18 +4,26 @@ class CustomInputField extends StatelessWidget {
   final String? hintText;
   final String? labelText;
   final String? helperText;
+  final IconData? iconData;
+  final TextInputType? keyboardType;
+  final bool? hidePassword;
 
   const CustomInputField({
     super.key,
     this.hintText,
     this.labelText,
     this.helperText,
+    this.iconData,
+    this.keyboardType,
+    this.hidePassword,
   });
 
   @override
   Widget build(BuildContext context) {
     return TextFormField(
       autofocus: true,
+      keyboardType: keyboardType,
+      obscureText: hidePassword ?? false,
       initialValue: '',
       textCapitalization: TextCapitalization.words,
       onChanged: (value) {
@@ -30,8 +38,8 @@ class CustomInputField extends StatelessWidget {
         hintText: hintText,
         label: labelText == null ? null : Text(labelText.toString()),
         helperText: helperText,
-        counterText: ' /20 caracteres',
-        icon: const Icon(Icons.person),
+        counterText: '',
+        icon: Icon(iconData),
 
         // counterStyle: ,
       ),
